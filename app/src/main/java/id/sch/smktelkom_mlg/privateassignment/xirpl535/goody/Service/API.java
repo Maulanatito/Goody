f885bdaa7d25cf642715d4d63c9d7f991befc928
Maulana Tito <titomaulana98@gmail.com>
2017-05-15 13:00:25 +0700

    First Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl535/goody/Service/API.java
@@ -0,0 +1,6 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl535.goody.Service;
+
+public class API {
+    static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static public String API_KEY = "d07e3c1ffeba29d658d879c531524df1";
+}
